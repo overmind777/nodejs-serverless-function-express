29236210677a24c4d04bd681ccff3ddefbd18833
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-03-26 00:35:03 +0200

    add db

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
