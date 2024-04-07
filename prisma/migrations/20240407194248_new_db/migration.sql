ddd7588550905e801944ff47e4e6342ded109f9d
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-04-07 22:46:10 +0300

    update schema prisma

--- prisma/migrations/20240407194248_new_db/migration.sql
+++ prisma/migrations/20240407194248_new_db/migration.sql
@@ -2,8 +2,9 @@
 CREATE TABLE "Tasks" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
+    "date" TEXT NOT NULL,
     "importance" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
